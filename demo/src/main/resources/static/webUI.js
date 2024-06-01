7fa64b56ad9245a0f109aca1b6640c3ba1e4908b
fka.varo <by.fka.varo@gmail.com>
2024-06-01 20:01:22 +0200

    Hide Cheats button

--- demo/src/main/resources/static/webUI.js
+++ demo/src/main/resources/static/webUI.js
@@ -1,6 +1,7 @@
 // When page is loaded
 document.addEventListener('DOMContentLoaded', () => {
     // Divisions
+    const cheats = document.getElementById('cheats');
     const userEnterButtons = document.getElementById('user-enter');
     const userActionButtons = document.getElementById('user-actions');
     const gameContainer = document.getElementById('game-container');
@@ -10,12 +11,22 @@ document.addEventListener('DOMContentLoaded', () => {
     const signOutButton = document.getElementById('signOut-button');
     const playButton = document.getElementById('play-button');
     const exitButton = document.getElementById('exit-button');
+    const cheatsButton = document.getElementById('cheats-button');
 
     //Headers
     var usernameHeader = document.getElementById('username-header');
 
     // Paragraphs
     var victoriesCount = document.getElementById('victories-count');
+
+    cheatsButton.addEventListener('click', () => {
+        if (cheats.style.display == 'none'){
+            cheats.style.display = 'block';
+        }
+        else {
+            cheats.style.display = 'none';
+        }
+    });
 
     // Sign OUT
     signOutButton.addEventListener('click', () => {
